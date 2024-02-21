bf8c4ec13a9e3a08f35692c5fa6ad3742130f6d0
Cody Rushing <codyrushing@gmail.com>
2024-02-20 23:38:42 -0600

    WIP storyblok integration

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,7 +1,36 @@
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import storyblok from '@storyblok/astro';
+
+const { STORYBLOK_TOKEN } = process.env;
+
+if(!STORYBLOK_TOKEN){
+  throw new Error(`STORYBLOK_TOKEN is a required env variable`);
+}
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()]
+    integrations: [
+        tailwind(),
+        storyblok({
+            accessToken: STORYBLOK_TOKEN,
+            components: {
+                page: 'storyblok/page',
+                hero: 'storyblok/hero',
+                markdown_content: 'storyblok/markdown-content',
+                richtext_content: 'storyblok/richtext-content'
+            },
+            apiOptions: {
+              // Choose your Storyblok space region
+              region: 'us', // optional,  or 'eu' (default)
+            }
+          })        
+    ],
+    vite: {
+        plugins: [basicSsl()],
+        server: {
+          https: true,
+        },
+      },
 });
